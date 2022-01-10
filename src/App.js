95e14a1f5040f9f23b32228348bd8c3f6d6516a5
Mark <mjbc53@gmail.com>
2022-01-09 19:59:06 -0600

    complete layout for head and footer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
+      <main>
+
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,1 +1,21 @@
 import React from "react";
+import githubIcon from '../../assets/images/icons/github.png'
+import linkedInIcon from '../../assets/images/icons/linkedin-logo.png'
+
+function Footer(){
+  return (
+    <footer class="d-flex justify-content-center py-4 flex-wrap">
+    <a  href="https://github.com/mjbc53"  rel="noreferrer" class="px-5" target="_blank">
+      <img class="icon-size" src={githubIcon} alt="github icon"/>
+    </a>
+    <a href="https://www.linkedin.com/in/mark-carriveau-6a9516217/"  rel="noreferrer" class="px-5" target="_blank">
+      <img class="icon-size" src={linkedInIcon} alt="linkedIn icon" />
+    </a>
+    <p class="w-100 text-center py-3 fs-4 black-text">
+      Made by Mark Carriveau. Thank you for visiting!
+    </p>
+</footer>
+  )
+}
+
+export default Footer
